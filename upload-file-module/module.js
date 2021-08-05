d3a9b2f67bb48717107bdef2f7e71df62b0f426b
Amit Khobragade <amitmusicstar@gmail.com>
2021-08-05 23:39:47 +0530

    needed to change some event handlers for desired output

--- upload-file-module/module.js
+++ upload-file-module/module.js
@@ -1,6 +1,9 @@
 // * Custom events for clicking on add or closing the prompt
-const addEvent = new Event("add-clicked");
-const closeEvent = new Event("close-clicked");
+const addEvent = new Event("addClicked");
+const closeEvent = new Event("closeClicked");
+
+const inpElem = document.getElementById("file-input");
+const dropBox = inpElem.parentElement;
 
 //close event
 document.querySelectorAll("#shade,#close-btn").forEach((e) => {
@@ -13,10 +16,8 @@ document.getElementById("add-btn").addEventListener("click", (e) => {
     document.dispatchEvent(addEvent);
 });
 
-const inpElem = document.getElementById("file-input");
-const dropBox = inpElem.parentElement;
-
 dropBox.addEventListener("click", (e) => {
+    var list = inpElem.files;
     inpElem.click();
 });
 
@@ -41,30 +42,45 @@ dropBox.addEventListener("dragover", (e) => {
 //!Donot Touch the function unless you know what you are doing
 // ! Almost the entire functioning of the module is dependent on
 // ! this peice of code so use with caution.
+var feildOccupied = false;
 inpElem.addEventListener("change", (e) => {
     var feild = document.querySelector(".text-area");
-    if (inpElem.files.length == 1) {
-        inpElem.previousElementSibling.classList.toggle("invisible");
-        inpElem.nextElementSibling.classList.toggle("invisible");
+    if (!feildOccupied && inpElem.files.length > 0) {
+        inpElem.previousElementSibling.classList.add("invisible");
+        inpElem.nextElementSibling.classList.remove("invisible");
         feild = document.createElement("div");
-        feild.classList.toggle("text-area");
+        feild.classList.add("text-area");
         dropBox.prepend(feild);
+        feildOccupied = true;
+    } else if (feildOccupied && inpElem.files.length == 0) {
+        feild.remove();
+        inpElem.previousElementSibling.classList.remove("invisible");
+        inpElem.nextElementSibling.classList.add("invisible");
+        feildOccupied = false;
     }
-    feild.querySelectorAll(".fileName").forEach((toDelete) => {
-        toDelete.remove();
-    });
 
-    // ! Warning: Changing 90% of the code below leads the code to break
-    for (var i = 0; i < inpElem.files.length; ++i) {
-        var name = inpElem.files[i].name;
-        const txt = document.createTextNode("\n" + name);
-        const node = document.createElement("h1");
-        node.append(txt);
-        node.classList.toggle("fileName");
-        feild.prepend(node);
+    if (feildOccupied) {
+        feild.querySelectorAll(".fileName").forEach((toDelete) => {
+            toDelete.remove();
+        });
+
+        // ! Warning: Changing 90% of the code below leads the code to break
+        for (var i = 0; i < inpElem.files.length; ++i) {
+            var name = inpElem.files[i].name;
+            const txt = document.createTextNode("\n" + name);
+            const node = document.createElement("h1");
+            node.append(txt);
+            node.classList.toggle("fileName");
+            feild.prepend(node);
+        }
     }
 });
 
 function getInputFiles() {
     return inpElem.files;
+}
+
+function clearInputStream() {
+    inpElem.value = null;
+    inpElem.dispatchEvent(new Event("change"));
 }
